1aecd7aaaec777a607e9360f2f48013646f0599a
maxmay94 <maxmay94@gmail.com>
2022-06-03 20:24:10 -0400

    add UI for add todo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from "react";
 // Pages
 import Index from "./pages/Index/Index";
+import AddTodo from "./components/AddTodo/AddTodo"
 // Styles
 import "./tailwind.output.css";
 
@@ -14,9 +15,8 @@ const App = () => {
       <div className="w-3/4 mx-auto shadow mt-10 px-4 py-4 rounded border border-gray-300 flex">
         <h1 className="font-bold flex-1">Todo List</h1>
 
-        <button className="bg-green-500 hover:bg-green-600 p-1 rounded">Add Todo</button>
-
       </div>
+        <AddTodo />
         <Index />
 
     </div>
---
+++ src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+
+const AddTodo = (props) => {
+
+  // const[todo, setTodo] = useEffect('')
+
+  return (
+    <div className="w-3/4 mx-auto shadow mt-10 px-4 py-4 rounded border border-gray-300 flex">
+      <form action="" className='w-full'>
+        <div className='px-5'>
+          <label className='font-bold text-xl'>Enter Todo Item</label>
+          <input
+            className='w-full'
+            required
+            name='todoItem'
+            autoComplete='off'
+            placeholder='What do you need to do?'  
+          />
+        </div>
+      </form>
+      <button className="bg-green-500 hover:bg-green-600 p-1 rounded">Add Todo</button>
+    </div>
+  )
+}
+
+export default AddTodo
